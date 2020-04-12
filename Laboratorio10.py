3006c3862c8972b2249fe20e83117d7ac3b46118
SilviaPabon <silvia.pabon.2020@users.noreply.github.com>
2020-04-12 15:46:16 -0500

    Modificación calcula n cantidad de potencias, arreglos bases y exponentes

--- Laboratorio10.py
+++ Laboratorio10.py
@@ -6,7 +6,16 @@ Created on Fri Apr  3 10:57:27 2020
 """
 # Informe laboratorio 10 - Desarrollo en Python
   
-# Desarrollo ejerc. 7 potencia, sin usar ** ni librería math
+# -------- Paquetes y librerías -------- #
+
+import numpy as np
+
+# Funciones #
+
+# zeros(n) retorna un arreglo con n cantidad de ceros cuando sea invocada
+
+def zeros(n):
+       return np.zeros(n)
 
 # a_power_b retorna al ser invocada, el resultado de elevar "a" a la "b"
     #Se usa un for para multiplicar "a" por sí mismo, "b" cantidad de veces
@@ -22,32 +31,45 @@ def a_power_b(a, b):
             acumul *= a
     return acumul
 
-# Desarrollo ejerc. 9.2
-    # a_power_b_bucle sigue calculando todas las potencias que el usario pida y se detiene
-    # cuando el usuario introduce a = 0
-        #Se usa un while, que se ejecutara hasta que a sea 0
-        #Adicionalmente ahora cuenta el número de potencias realizadas 
-        #y cuántas veces el resultado fue para o impar
-def a_power_b_bucle():
+# Desarrollo ejerc. 9.3, 9.3.1
+    # a_power_b_bucle ha sido modificada para que el usuario ingrese
+    # el número de parejas de potencias que desea resolver, que mínimo serán 5
+    # la función termina la ejecución cuando se hayan ingresado n número de parejas
+        # 9.3.1 a_power_b_bucle al invocar la función zeros(n) forma 2 arreglos
+        # de n ceros, según el n inicial
+            #Se usa for para que el valor de array_a o b cambie en cada vuelta
+    
+def a_power_b_bucle(n):    
     contador = 0
     par = 0
     impar = 0
-    while True:
-        a = int(input("Ingresar el valor de la base: "))
-        if not a:
-            print("Ha introducido base = 0. FIN.")
-            print("Ud hizo", contador, "potencias")
-            print("Ud hizo", par, "potencias pares")
-            print("Ud hizo", impar, "potencias impares")
-            break
-        b = int(input("valor de el exponente: "))
-        c = a_power_b(a, b)
-        #Si a no es 0, se ejecutará también la función a_power_b invocada        
-        contador +=1
-        print("La potencia de " + str(a) + " elevado a la " + str(b) + " es: " + str(c))
-        if (c%2 == 0):
-            par += 1
-        elif (c%2 == 1):
-            impar += 1
+    # func. zeros(n) invocada
+    array_a = zeros(n)
+    array_b = zeros(n)
+    if n>=5:
+        for i in range(n):
+            a = int(input("Ingresar el valor de la base: "))
+            b = int(input("valor de el exponente: "))
+            # cambio de valor en arreglo, según a y b ingresados en vuelta i
+            array_a [i] = a
+            array_b [i] = b
+            c = a_power_b(a, b)
+            contador +=1
+            #Impresión potencias
+            print("La potencia de " + str(a) + " elevado a la " + str(b) + " es: " + str(c))
+            if (c%2 == 0):
+                par += 1
+            elif (c%2 == 1):
+                impar += 1
+        print("Ud hizo", contador, "potencias")
+        print("Ud hizo", par, "potencias pares")
+        print("Ud hizo", impar, "potencias impares")
+    else:
+        print("Ud quiere calcular menos de 5 potencias. FIN.")
+    #Impresión arreglos
+    print ("Arreglo de bases", array_a)
+    print ("Arreglo de exponentes", array_b)
 
-a_power_b_bucle()
+# n es la cantidad de potencias que se calcularán
+n = int(input("Ingresar una cantidad a calcular: "))
+a_power_b_bucle(n)
